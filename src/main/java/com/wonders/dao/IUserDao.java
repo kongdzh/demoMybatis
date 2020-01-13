5278dc7e6bd586c605c7914dc931c533e212b8dd
kongdzh <dennis_kong@icloud.com>
2020-01-13 17:30:47 +0800

    @Select 注解方式

--- src/main/java/com/wonders/dao/IUserDao.java
+++ src/main/java/com/wonders/dao/IUserDao.java
@@ -1,6 +1,7 @@
 package com.wonders.dao;
 
 import com.wonders.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface IUserDao {
     /**
      * 查询所有
      */
+    @Select("SELECT * FROM user;")
     List<User> findAll();
 }
